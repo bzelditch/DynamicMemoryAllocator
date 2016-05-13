18b85f63ae05b408eafeb387e9219e88cd8efee6
jtliao <jtliao9@yahoo.com>
2016-05-13 16:29:15 +0000

    Added some more tests

--- heaplib.c
+++ heaplib.c
@@ -7,25 +7,47 @@
  *
  */
 
+/* Sets up a new heap.  'heapptr' points to a chunk of memory (i.e. the heap)
+ * of size 'heap_size' bytes.  Returns 0 if setup fails, nonzero if success.
+ */
 int hl_init(void *heapptr, unsigned int heap_size) {
 
-  return 0; // Failed
+  	return 0; // Failed
 
 }
 
+/* Allocates a block of memory of the given size from the heap starting at 
+ * 'heapptr'. Returns a pointer to the block of memory on success; returns 0 
+ * if the allocator cannot satisfy the request.
+ */
 void *hl_alloc(void *heapptr, unsigned int block_size) {
-
-  return NULL; // Failed
-
+  	if(block_size < 0){
+  		return NULL;
+  	}
+  	return NULL; // Failed
 }
 
+/* Releases the block of previously allocated memory pointed to by blockptr.
+ * Nop if blockptr == 0.
+ */
 void hl_release(void *heapptr, void *blockptr) {
+	if(blockptr == 0){
+		return;
+	}
+
 
 }
 
+/* Changes the size of the memory block pointed to by blockptr,
+ * returning a pointer to the new block, or 0 if the request cannot be
+ * satisfied. The contents of the block should be preserved (even if
+ * the location of the block changes).  If mem == 0, function should
+ * behave like alloc().
+ */
 void *hl_resize(void *heapptr, void *blockptr, unsigned int new_block_size) {
-
-  return NULL; // Failed
-
+  	if(block_size < 0){
+  		return NULL;
+ 	}
+  	return NULL; // Failed
 }
 
--- test_c.c
+++ test_c.c
@@ -10,6 +10,7 @@
 // when testing for correctness make heap nice and big
 // don't want bloated meta data to be the problem
 #define DECENT_HEAP_SIZE 256
+#define SMALL_HEAP_SIZE 32
 
 int num_tests = 0;
 
@@ -77,32 +78,94 @@ int alloc_ptr_aligned() {
 }
 
 /**
- * Test for failures in incorrect malloc call
+ * Test for failures in incorrect alloc call
  */
-int test() {
+int alloc_failure() {
 	char array[DECENT_HEAP_SIZE]; 
 	void *block;
 	bool aligned_f = false;
+	num_tests++; // begin each test by incrementing this counter
+	hl_init(&array, DECENT_HEAP_SIZE - 1);
+	block = hl_alloc(&array, 500);
+	aligned_f = block != 0;
+
+	printf("%d) hl_alloc catches failure case?", num_tests); 
+	if (!aligned_f) {
+		printf("\t\tPASS\n");
+		return SUCCESS;
+	}
+	
+	printf("\t\tFAIL\n"); 
+	return FAIL;
+}
+
+/**
+ * Test for failures in incorrect init call
+ */
+int init_failure() {
+	char array[DECENT_HEAP_SIZE]; 
+	bool failure = false;
+	int size = 4;
+	num_tests++; // begin each test by incrementing this counter
+	failure = hl_init(&array, size);
+
+	printf("%d) hl_init catches failure case?", num_tests); 
+	if (!failure) {
+		printf("\t\tPASS\n");
+		return SUCCESS;
+	}
+	printf("\t\tFAIL\n"); 
+	return FAIL;
+}
+
+/**
+ * Test for failures in incorrect malloc call
+ */
+int resize_failure() {
+	char array[DECENT_HEAP_SIZE]; 
+	void *block;
+	void *resized;
+	bool failure = false;
+
 
 	num_tests++; // begin each test by incrementing this counter
 
 	hl_init(&array, DECENT_HEAP_SIZE - 1);
-	
 	block = hl_alloc(&array, 11);
+	resized = hl_resize(&array, block, 500);
+	failure = resized != 0;
 
-	// you may find this helpful. feel free to remove
-    #ifdef PRINT_DEBUG
-    	printf("blockptr = %16lx\n", (unsigned long)block);
-    	printf("mask =     %16lx\n", (unsigned long)(ALIGNMENT -1));
-    	printf("---------------------------\n");
-    	printf("ANDED =    %16lx\n", (unsigned long)block & (ALIGNMENT - 1));
-    	printf("!ANDED (ALIGNED) =   %6d\n", !((unsigned long)block & (ALIGNMENT - 1)));
-    #endif
+	printf("%d) hl_resize catches failure case?", num_tests); 
+	if (!failure) {
+		printf("\t\tPASS\n");
+		return SUCCESS;
+	}
+	printf("\t\tFAIL\n"); 
+	return FAIL;
+}
 
-	aligned_f = !((unsigned long)block & (ALIGNMENT - 1));
+int heap_overflow() {
+	char heap[SMALL_HEAP_SIZE]; 
+	char *letters; 
+	void *new_letters;
 
-	printf("%d) hl_alloc return ptr has correct alignment?", num_tests); 
-	if (aligned_f) {
+	num_tests++; // begin each test by incrementing this counter
+
+	hl_init(heap, SMALL_HEAP_SIZE);
+	letters = (char *) hl_alloc(heap, 6 * sizeof(char));
+
+	//printf("%lu\n", sizeof(char));
+	letters[0] = 'h';
+	letters[1] = 'e';
+	letters[2] = 'l';
+	letters[3] = 'l';
+	letters[4] = 'o';
+	letters[5] = '\0';
+
+	new_letters = hl_alloc(heap, 28 * sizeof(char));
+
+	printf("%d) Heap doesn't overflow with alloc call?", num_tests); 
+	if (new_letters == 0) {
 		printf("\tPASS\n");
 		return SUCCESS;
 	}
@@ -111,33 +174,29 @@ int test() {
 	return FAIL;
 }
 
-/**
- * Test for failures in incorrect init call
- */
-int test2() {
-	char array[DECENT_HEAP_SIZE]; 
-	void *block;
-	bool aligned_f = false;
+int release_test() {
+	char heap[SMALL_HEAP_SIZE]; 
+	char *letters; 
+	void *new_letters;
 
 	num_tests++; // begin each test by incrementing this counter
 
-	hl_init(&array, DECENT_HEAP_SIZE - 1);
-	
-	block = hl_alloc(&array, 11);
+	hl_init(heap, SMALL_HEAP_SIZE);
+	letters = (char *) hl_alloc(heap, 6 * sizeof(char));
 
-	// you may find this helpful. feel free to remove
-    #ifdef PRINT_DEBUG
-    	printf("blockptr = %16lx\n", (unsigned long)block);
-    	printf("mask =     %16lx\n", (unsigned long)(ALIGNMENT -1));
-    	printf("---------------------------\n");
-    	printf("ANDED =    %16lx\n", (unsigned long)block & (ALIGNMENT - 1));
-    	printf("!ANDED (ALIGNED) =   %6d\n", !((unsigned long)block & (ALIGNMENT - 1)));
-    #endif
+	//printf("%lu\n", sizeof(char));
+	letters[0] = 'h';
+	letters[1] = 'e';
+	letters[2] = 'l';
+	letters[3] = 'l';
+	letters[4] = 'o';
+	letters[5] = '\0';
 
-	aligned_f = !((unsigned long)block & (ALIGNMENT - 1));
+	hl_release(heap, letters);
+	new_letters = hl_alloc(heap, 12 * sizeof(char));
 
-	printf("%d) hl_alloc return ptr has correct alignment?", num_tests); 
-	if (aligned_f) {
+	printf("%d) Heap correctly allocates after release?", num_tests); 
+	if (new_letters != 0) {
 		printf("\tPASS\n");
 		return SUCCESS;
 	}
@@ -154,7 +213,11 @@ int main() {
 	
 	num_passes += resize_copies_values();
 	num_passes += alloc_ptr_aligned();
-	
+	num_passes += alloc_failure();
+	num_passes += init_failure();
+	num_passes += resize_failure();
+	num_passes += heap_overflow();
+	num_passes += release_test();
 	
 	printf("Num_Passed_Tests: %d\n", num_passes);
 	printf("Num_Tests: %d\n", num_tests);
